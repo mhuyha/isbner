9c67560f1bf8f322a384b8fd0d6259f76b159243
Minh Huy Ha <minhha@gmail.com>
2012-12-14 21:57:26 -0500

    + isbn regex pattern matching

--- isbner.rb
+++ isbner.rb
@@ -1,3 +1,6 @@
+require 'pdf/reader'
+
+
 class ISBNer
 
 
@@ -6,8 +9,45 @@ class ISBNer
 	end
 
 	def list_files
-		puts @pdf_files
+		puts @pdf_files[1]
 	end
+
+	def find_isbn(filename)
+		file = Dir.pwd << "/#{filename}"
+
+		# isbn 9 and 13 regex pattern
+		isnbn_13 = /ISBN(-1(?:(0)|3))?:?\x20(\s)*[0-9]+[- ][0-9]+[- ][0-9]+[- ][0-9]*[- ]*[xX0-9]/
+
+		# array to hold extracted isbns
+		isbns = []
+
+		PDF::Reader.open(file) do |reader|
+
+			reader.pages[1..10].each do |page|
+		    isbn_matches = page.text.match isnbn_13
+		    isbns.push(isbn_matches) if isbn_matches
+			end
+
+			reader.pages[(reader.page_count-20)..reader.page_count].each do |page|
+		    isbn_matches = page.text.match isnbn_13
+		    isbns.push(isbn_matches) if isbn_matches
+		    puts page.text
+			end
+
+
+			puts isbns
+
+		end
+
+
+	end
+
+	def process_pdfs
+
+		find_isbn(@pdf_files[1])
+
+	end
+
 
 end
 
@@ -15,3 +55,5 @@ end
 pdfbooks = ISBNer.new
 
 pdfbooks.list_files
+
+pdfbooks.process_pdfs
